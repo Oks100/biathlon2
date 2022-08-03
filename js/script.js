f4d46a0d9dec38b3c66e433bd4b7d49fa4b7fa18
Oks100 <79767723+Oks100@users.noreply.github.com>
2022-08-03 19:45:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+new Swiper('.swiper-container', {
+  // Вывод стрелок навигации
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ pagination: {
+   el: '.swiper-pagination',
+ },
+   // Вывод скроллбара
+ scrollbar: {
+   el: '.swiper-scrollbar',
+    draggable: true,
+ },
+   // Прогрессбар
+  pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar'
+  },
+  loop: true,
+  preloadImages: false,
+  // Lazy Loading (подзагрузка картинок)
+  lazy: {
+    // Подзагрузка на старте перключения слайда
+    loadOnTransitionStart: true,
+    // Подгрузить предыдущую и следующую картинки
+    loadPrevNext: true,
+  },
+  effect: 'flip',
+  flipEffect: {
+    // Тень
+    slideShadows: true,
+    // Показ только активного слайда
+    limitRotation: true,
+  },
+ });
+
 /*$(document).ready(function() {
     $('.header__burger').click(function(event){
         $('.header__burger,.header__menu').toggleClass('active');
@@ -12,9 +49,10 @@ checkbox.addEventListener('change', ()=>{
 });*/
 
 
-const swiper = new Swiper('.swiper', {
+/*const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
+  slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination',
     type: 'progressbar'
@@ -23,4 +61,4 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-});
+});*/
